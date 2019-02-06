25af2494e9ebfc34452839daea85df8423e444a3
松本望実 <nozomi@nozomi.local>
2019-02-06 19:11:30 +0900

    [Finish]5-2

--- Gemfile
+++ Gemfile
@@ -62,4 +62,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # Gem devise導入
-gem 'device'
+gem 'devise'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  # nameカラム追加に伴うdevise内アクションへの修正
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
